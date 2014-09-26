9ed27da5b7e6303609f042f84382cd3d82d82403
Emil Edholm <emil@edholm.it>
2014-09-26 23:17:04 +0200

    Now outputs valid i3bar json

--- src/status-to-json.cc
+++ src/status-to-json.cc
@@ -28,16 +28,22 @@ int main(int argc, char *argv[])
     signal(SIGUSR1, signal_handler);
     signal(SIGUSR2, signal_handler);
     vector<unique_ptr<Generator>> generators;
-    generators.push_back(unique_ptr<CPU>(new CPU()));
-    generators.push_back(unique_ptr<Memory>(new Memory()));
-    generators.push_back(unique_ptr<Wifi>(new Wifi()));
-    generators.push_back(unique_ptr<Battery>(new Battery()));
-    generators.push_back(unique_ptr<Volume>(new Volume()));
+    //generators.push_back(unique_ptr<CPU>(new CPU()));
+    //generators.push_back(unique_ptr<Memory>(new Memory()));
+    //generators.push_back(unique_ptr<Wifi>(new Wifi()));
+    //generators.push_back(unique_ptr<Battery>(new Battery()));
+    //generators.push_back(unique_ptr<Volume>(new Volume()));
     generators.push_back(unique_ptr<DateTime>(new DateTime()));
 
+    cout << "{\"version\" : 1} [ [],";
     while(true) {
-        for(unsigned int i = 0; i < generators.size(); i++)
-            cout << generators.at(i)->generate_json() << endl;
+        cout << "[";
+        for(unsigned int i = 0; i < generators.size(); i++) {
+            cout << generators.at(i)->generate_json();
+        }
+        cout << "],";
+        
+        fflush(stdout);
         sleep(2);
     }
 }
