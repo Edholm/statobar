3c731c1abca4c96e29d2d5b4101f83771a24d2cd
Emil Edholm <emil@edholm.it>
2014-09-26 13:32:47 +0200

    Added color defines

--- common.hh
+++ common.hh
@@ -1,6 +1,10 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
+#define COLOR_DEFAULT "#839496"
+#define COLOR_CRIT "#c00000"
+#define COLOR_WARN "#b58900"
+
 class Common {
 public:
     static std::string read_first_line(std::string);
