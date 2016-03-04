e1234ea6374290cb576a010b08d0631b47e43626
Emil Edholm <bobby@verypublic.org>
2016-03-04 21:17:17 +0100

    Show connected BT devices

--- includes/bluetooth.hh
+++ includes/bluetooth.hh
@@ -4,18 +4,25 @@
 #pragma once
 #ifndef __BLUETOOTH_H__
 #define __BLUETOOTH_H__
+#include <gio/gio.h>
+#include <vector>
 #include "generator.hh"
 
 class Bluetooth: public Generator {
 private:
-    const char* INTERFACE          = "hci0";
-    const char* BLUEZ_NAME         = "org.bluez";
-    const char* ADAPTER            = "org.bluez.Adapter1";
-    const char* BLUEZ_OBJECT_PATH  = "/org/bluez/hci0";
-    const char* DBUS_PROP_INT_NAME = "org.freedesktop.DBus.Properties";
+    const std::string INTERFACE          = "hci0";
+    const std::string BLUEZ_NAME         = "org.bluez";
+    const std::string ADAPTER            = BLUEZ_NAME + ".Adapter1";
+    const std::string DEVICE             = BLUEZ_NAME + ".Device1";
+    const std::string BLUEZ_OBJECT_PATH  = "/org/bluez/" + INTERFACE;
+    const std::string DBUS_PROP_INT_NAME = "org.freedesktop.DBus.Properties";
 
+    GDBusProxy* dbus_get_bus(const gchar*, const gchar*, GError*);
+    GVariant* dbus_call_method(GDBusProxy*, const gchar*, GVariant*, GError*);
+    std::vector<std::string> dbus_list_devices(GDBusProxy*);
+    std::vector<std::string> connected_names(std::vector<std::string>*);
     /* Whether or not the bluetooth adapter is powered */
-    bool is_powered();
+    bool is_powered(GDBusProxy*);
 public:
     std::string generate_json();
 };
--- src/bluetooth.cc
+++ src/bluetooth.cc
@@ -3,7 +3,9 @@
  *
  * Copyright 2016 Emil Edholm <emil@edholm.it>
  */
+#include <boost/algorithm/string/join.hpp>
 #include <gio/gio.h>
+#include <vector>
 #include "bluetooth.hh"
 #include "common.hh"
 
@@ -11,35 +13,69 @@ using namespace std;
 
 #define BLUETOOTH_ICON "\uf293"
 
-bool Bluetooth::is_powered() {
-    GError *err = NULL;
-    GDBusProxy* bus_proxy =
-        g_dbus_proxy_new_for_bus_sync(
+GDBusProxy* Bluetooth::dbus_get_bus(const gchar* object_path, const gchar* interface, GError* err) {
+    return g_dbus_proxy_new_for_bus_sync(
             G_BUS_TYPE_SYSTEM,
             G_DBUS_PROXY_FLAGS_NONE,
             NULL,
-            BLUEZ_NAME,
-            BLUEZ_OBJECT_PATH,
-            DBUS_PROP_INT_NAME,
+            BLUEZ_NAME.c_str(),
+            object_path,
+            interface,
             NULL,
             &err);
+}
 
-    if(err) {
-        return false;
-    }
-    err = NULL;
-    GVariant *var =
-        g_dbus_proxy_call_sync(
-            bus_proxy,
-            "Get",
-            g_variant_new("(ss)",
-                ADAPTER,
-                "Powered"),
+GVariant* Bluetooth::dbus_call_method(GDBusProxy* bus, const gchar* method, GVariant *params, GError *err) {
+    return g_dbus_proxy_call_sync(
+            bus,
+            method,
+            params,
             G_DBUS_CALL_FLAGS_NONE,
             -1,
             NULL,
             &err);
+}
 
+vector<string> Bluetooth::dbus_list_devices(GDBusProxy *bus) {
+    vector<string> devices;
+    GError *err = NULL;
+
+    GVariant *result = dbus_call_method(bus,
+            "org.freedesktop.DBus.Introspectable.Introspect",
+            NULL,
+            err);
+    if(err) {
+        return devices;
+    }
+
+    const gchar *xml_data;
+    g_variant_get(result, "(&s)", &xml_data);
+
+    err = NULL;
+    GDBusNodeInfo *nodes =
+        g_dbus_node_info_new_for_xml (xml_data, &err);
+    if(err) {
+        return devices;
+    }
+
+    GDBusNodeInfo **device_node_info = nodes->nodes;
+    while(*device_node_info) {
+        devices.push_back(string((*device_node_info)->path));
+        device_node_info++;
+    }
+
+    g_variant_unref(result);
+    return devices;
+}
+
+bool Bluetooth::is_powered(GDBusProxy *bus) {
+    GError *err = NULL;
+    GVariant *var =
+        dbus_call_method(bus, "Get",
+            g_variant_new("(ss)",
+                ADAPTER.c_str(),
+                "Powered"),
+            err);
 
     if(err) {
         return false;
@@ -48,23 +84,66 @@ bool Bluetooth::is_powered() {
     g_variant_get(var, "(v)", &value);
 
     gboolean powered = g_variant_get_boolean(value);
-    g_object_unref(bus_proxy);
     g_variant_unref(var);
     g_variant_unref(value);
     return powered;
 }
 
+/* Get the "pretty" names of all connected bluetooth devices */
+vector<string> Bluetooth::connected_names(vector<string>* devices) {
+    vector<string> names;
+    GError* err = NULL;
+    for(auto d : *devices) {
+        string obj_path = BLUEZ_OBJECT_PATH + "/" + d;
+        GDBusProxy *bus = dbus_get_bus(obj_path.c_str(), DBUS_PROP_INT_NAME.c_str(), err);
+
+        err = NULL;
+        GVariant *var =
+            dbus_call_method(bus, "GetAll", g_variant_new("(s)", DEVICE.c_str()), err);
+        if(err) {
+            // FIXME: log maybe baby
+            continue;
+        }
+        GVariant *value = g_variant_get_child_value(var, 0);
+        GVariantDict *props = g_variant_dict_new(value);
+
+        gboolean connected;
+        g_variant_dict_lookup(props, "Connected", "b", &connected);
+        if(connected) {
+            const gchar* name;
+            g_variant_dict_lookup(props, "Name", "s", &name);
+            names.push_back(string(name));
+        }
+
+        g_variant_unref(var);
+        g_variant_unref(value);
+        g_variant_dict_unref(props);
+        g_object_unref(bus);
+    }
+    return names;
+}
+
 string Bluetooth::generate_json() {
-    bool powered = is_powered();
+    GError *err = NULL;
+    GDBusProxy* bus = dbus_get_bus(BLUEZ_OBJECT_PATH.c_str(), DBUS_PROP_INT_NAME.c_str(), err);
+    if(err) {
+        return "";
+    }
+
+    bool powered = is_powered(bus);
     if(!powered) {
         return "";
     }
+
+    vector<string> devices = dbus_list_devices(bus);
+    g_object_unref(bus);
+    string joinedNames = boost::algorithm::join(connected_names(&devices), ", ");
 
     string status = BLUETOOTH_ICON;
     string filler = Common::filler_json("");
 
     map<string, string> m;
-    m["full_text"] = status + " ";
+    m["full_text"] = "  " + status + " " + joinedNames + " ";
 
     return filler + ", " + Common::map_to_json(m);
 }
